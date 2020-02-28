637d977337a457a9943845a3b3996d5343db86d3
Mauricio Araldi <araldi.mauricio@gmail.com>
2020-02-28 19:08:19 +0100

    Front-end getting dynamic info

--- src/app/exchanges/exchanges.component.ts
+++ src/app/exchanges/exchanges.component.ts
@@ -4,13 +4,12 @@ import { map } from 'rxjs/operators';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 
-const GET_EXCHANGES = gql`
+const GET_OPERATIONS = gql`
   subscription exchanges($type: [OperationType]) {
     operations(type: $type) {
-      id,
       transaction {
+        id,
         feeAmount,
-        feeCharged,
         success
       }
     }
@@ -23,17 +22,47 @@ const GET_EXCHANGES = gql`
   styleUrls: ['./exchanges.component.css']
 })
 export class ExchangesComponent implements OnInit {
-  exchanges: Observable<any>;
+  operations: any[] = [];
+  highestValue: number = null;
+  lowestValue: number = null;
+  averageValue: number = 0;
+  totalValue: number = 0;
 
   constructor(private apollo: Apollo) {}
 
   ngOnInit() {
     this.apollo.subscribe({
-        query: GET_EXCHANGES,
+        query: GET_OPERATIONS,
         variables: {
           type: 'payment'
         }
       }).subscribe(result => {
+        const { operations }: any = result.data;
+        const { transaction }: any = operations;
+
+        if (!transaction.success) {
+          return;
+        }
+
+        const operation = {
+          id: transaction.id,
+          value: transaction.feeAmount,
+          date: new Date(),
+        };
+
+        if (this.highestValue === null || operation.value > this.highestValue) {
+          this.highestValue = operation.value;
+        }
+
+        if (this.lowestValue === null || operation.value < this.lowestValue) {
+          this.lowestValue = operation.value;
+        }
+
+        this.totalValue += operation.value;
+
+        this.averageValue = Math.round(this.totalValue / this.operations.length);
+
+        this.operations.unshift(operation);
       });
   }
 
